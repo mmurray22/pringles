204319b8950cde75064224a33256da259d4e7c0a
curtfuffle <colangdon@berkeley.edu>
2025-05-24 13:13:28 -0700

    Finished up base corfu client code. Still need to work on adding to trace file for corfu packets and need to finish up corfu storage

--- code/include/corfu_client.h
+++ code/include/corfu_client.h
@@ -4,8 +4,9 @@
 #include <map>
 #include <cstdint>
 #include <cstddef>
+#include <chrono>
 #include "base_client.h"
-#include "flash_id.h"
+#include "corfu_storage.h"
 
 #define TIMEOUT 10
 #define CORFU_PROTO_TYPE 2
@@ -14,7 +15,6 @@ class CorfuClient : public BaseClient {
     protected:
         CorfuSequencer sequencer;
         uint64_t get_tail();
-        uint64_t reconfigure(uint64_t idx, );
         uint64_t curr_epoch = 0;
         
         std::vector<CorfuStorage> corfu_log;
@@ -25,7 +25,7 @@ class CorfuClient : public BaseClient {
     public:
         CorfuClient(YAML::Node config);
         uint64_t append(std::unique_ptr<std::string> entry);
-        std::unique_ptr<std::string> read(uint64_t idx);
-        uint64_t fill(uint64_t idx);
-        uint64_t trim(uint64_t idx);
+        std::unique_ptr<std::string> read(uint64_t log_idx);
+        uint64_t fill(uint64_t log_idx);
+        uint64_t trim(uint64_t log_idx);
 }
--- code/src/corfu_client.cpp
+++ code/src/corfu_client.cpp
@@ -142,69 +142,120 @@ CorfuClient::CorfuClient(YAML::Node config) {
 }
 
 uint64_t CorfuClient::get_tail() {
-    return sequencer.assign_next_idx;
+    return this->sequencer.assign_next_idx;
 }
 
 uint64_t CorfuClient::append(std::unique_ptr<std::string> entry) {
     uint64_t log_idx = get_tail();
     // loop through all of the replicas that have this log position
-    for (const auto& curr_range : auxiliary[curr_epoch].keySet()) {
-        if (curr_range.contains(log_idx)) {
-            sm = __
+    std::vector<CorfuStorage> send_machines;
+    // use current epoch to find corresponding send machines
+    for (const auto& config_kv : this->auxiliary[this->curr_epoch]) {
+        std::pair<uint64_t, uint64_t> curr_range = config_kv.first;
+        std::vector<CorfuStorage> server_range = config_kv.second;
 
-            std::unique_ptr<std::string> write_packet = serialize_str_entry(entry, CORFU_PROTO_TYPE);
-            net->add_to_send_queue(write_packet, sm.IP);
+        if (log_idx >= curr_range.first && log_idx < curr_range.second) {
+            send_machines = server_range;
+            break;
+        }
+    }
 
-            std::string msg;
-            // get curr time
-            while (time < TIMEOUT && msg == NULL) {
-                msg = net->read_from_recv_queue(sm.IP);
-            }
+    if (send_machines.empty()) {
+        spdlog::critical("Unable to find send machines");
+        return 1; // return 1 indicates there was a failure and we should try again
+    }
 
-            // must reconfigure if there's no response
-            if (msg == NULL) {
-                FlashID failing_unit = auxiliary[curr_epoch][log_idx][0];
-                reconfigure(log_idx, failingUnit);
-                return 1; // return 1 indicates there was a failure and we should try to append again
-            }
+    for (CorfuStorage sm : send_machines) {
+        std::unique_ptr<std::string> write_packet = serialize_str_entry(entry, CORFU_PROTO_TYPE);
+        net->add_to_send_queue(write_packet, sm.IP);
 
-            if (msg == err_sealed) {
-                FlashID failingUnit = axiliary[curr_epoch][log_idx][0];
-                reconfigure(log_idx, failing_unit);
-                return 1; // indicates there was a failure and we should try to append again
-            }
+        std::string msg;
 
-            if (msg == err_deleted || msg == err_unwritten) {
-                return msg;
-            }
+        // start timer
+        auto start_time = std::chrono::high_resolution_clock::now();
+        auto read_time = std::chrono::high_resolution_clock::duration::zero();
+        while (read_time < TIMEOUT && msg.empty()) {
+            msg = net->read_from_recv_queue(sm.IP);
+            read_time = std::chrono::high_resolution_clock::now() - start_time;
+        }
+
+        // must reconfigure if there's no response
+        if (msg.empty() || read_time < TIMEOUT) {
+            spdlog::info("Must reconfigure because there was no response");
+            CorfuStorage failing_unit = auxiliary[curr_epoch][log_idx][0];
+            reconfigure(log_idx, failing_unit);
+            spdlog::info("Just reconfigured, you should attempt to append again");
+            return 1; // return error
+        }
+
+        if (msg == err_sealed) {
+            spdlog::info("Must reconfigure because the current epoch was sealed");
+            CorfuStorage failing_unit = auxiliary[curr_epoch][log_idx][0];
+            reconfigure(log_idx, failing_unit);
+            spdlog::info("Just reconfigured, you should attempt to append again");
+            return 1; // return error
+        }
+
+        if (msg == err_deleted) {
+            spdlog::critical("We got an err_deleted and now we're returning the error code");
+            return err_deleted;
+        }
+
+        if (msg == err_unwritte) {
+            spdlog::critical("We got an err_unwritten and now we're returning the error code");
+            return err_unwritten;
         }
     }
     return log_idx;
 }
 
 std::unique_ptr<std::string> SimpleClient::read(uint64_t idx) {
-
+    return nullptr;
 }
 
 uint64_t CorfuClient::fill(uint64_t idx) {
-
+    return 0;
 }
 
-uint64_t CorfuClient::trim(uint64_t idx) {
-    for (const auto& curr_range : auxiliary[curr_epoch].keySet()) {
-        if (idx in curr_range) {
-            sm = __
+uint64_t CorfuClient::trim(uint64_t log_idx) {
+    // loop through all of the replicas that have this log position
+    std::vector<CorfuStorage> send_machines;
+    // use current epoch to find corresponding send machines
+    for (const auto& config_kv : this->auxiliary[this->curr_epoch]) {
+        std::pair<uint64_t, uint64_t> curr_range = config_kv.first;
+        std::vector<CorfuStorage> server_range = config_kv.second;
 
-            std::unique_ptr<std::string> delete_packet = serialize_str_entry(entry, CORFU_PROTO_TYPE);
-            net->add_to_send_queue(delete_packet, sm.IP);
-
-            std::string msg;
-            // get curr time
-            while (time < TIMEOUT && msg == NULL) {
-                msg = net->read_from_recv_queue(sm.IP);
-            }
+        if (log_idx >= curr_range.first && log_idx < curr_range.second) {
+            send_machines = server_range;
+            break;
         }
     }
+
+    if (send_machines.empty()) {
+        spdlog::critical("Unable to find send machines");
+        return 1; // return 1 indicates there was a failure and we should try again
+    }
+
+    for (CorfuStorage sm : send_machines) {
+        std::unique_ptr<std::string> delete_packet = serialize_str_entry("delete", CORFU_PROTO_TYPE);
+        net->add_to_send_queue(delete_packet, sm.IP);
+
+        std::string msg;
+        // start timer
+        auto start_time = std::chrono::high_resolution_clock::now();
+        auto read_time = std::chrono::high_resolution_clock::duration::zero();
+        while (read_time < TIMEOUT && msg.empty()) {
+            msg = net->read_from_recv_queue(sm.IP);
+            read_time = std::chrono::high_resolution_clock::now() - start_time;
+        }
+
+        // never received ack
+        if (msg.empty() || read_time < TIMEOUT) {
+            spdlog::critical("We did not receive an ack :(");
+            return 1; // failure
+        }
+    }
+    return 0; // success
 }
 
 int main(int argc, char* argv[]) {
@@ -223,6 +274,7 @@ int main(int argc, char* argv[]) {
     spdlog::info("Simple Network! Sending on localhost 127.0.0.1");
     std::shared_ptr<Trace<std::string>> trace = std::make_shared<Trace<std::string>>(get_trace_file(config));
 
+    // what to put here instead of custom_server, custom_client ?
     std::thread server_thread(custom_server, custom_net, trace);
     std::thread client_thread(custom_client, custom_net, trace);
     client_thread.join();
