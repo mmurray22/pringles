0df60f54d80852a6d9c9613e0e8461a6d6f446c6
curtfuffle <colangdon@berkeley.edu>
2025-10-14 04:10:36 -0700

    created test file for sequencer

---
+++ experiments/unit_tests/test_sequencer.cpp
@@ -0,0 +1,9 @@
+#include "network.h"
+#include "spdlog/spdlog.h"
+
+void main() {
+    /*Default parameters for testing*/
+
+    spdlog::debug("Testing one sequencer request");
+    spdlog::debug("Testing multiple sequencer requests");
+}
